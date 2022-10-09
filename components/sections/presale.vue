0fb2264ac8b5d6d8c2edc50dd8e3fd524b836e5d
web-dev-a <61269281+web-dev-a@users.noreply.github.com>
2022-10-09 21:34:51 +0300

    Update presale.vue

--- components/sections/presale.vue
+++ components/sections/presale.vue
@@ -4,6 +4,8 @@
       <div class="grid gap-20 content-between self-center">
         <div class="grid">
           <client-only>
+          <span class="uppercase typo-capital">Test
+          </span>
           <div class="grid gap-6 grid-flow-col items-center justify-start"><p>Lucky & Chucky are the characters of collection which contains 10 000 fabulous avatars inspired by childhood, hand drawn in pretty pastels and adorned with cute detailing. Super cute and child safe artworks breath new life into the rude and weird world of NFTs. These friends are sure to be a hit and make a lovely addition to any NFT collection. Artworks ware created using 100% renewable energy.
           </p></div>
           
