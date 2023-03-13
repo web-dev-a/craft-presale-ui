496ad45ba7ea262b2340af7129e1b5a3d7dd6732
web-dev-a <61269281+web-dev-a@users.noreply.github.com>
2023-03-13 10:35:28 +0200

    Update Header.vue

--- components/partials/navigation/Header.vue
+++ components/partials/navigation/Header.vue
@@ -1,93 +1,20 @@
 <template>
-  <header class="sticky top-0 py-10" style="background-color:#FFF">
-    <Container class="grid gap-20 grid-flow-col items-center justify-between">
-      <nuxt-link
-        to="https://olo.pics"
-        class="grid gap-10 grid-flow-col items-center justify-self-start"
-      >
-        <client-only>
-          <img
-            class="w-32 h-32 object-cover"
-            src="https://res.cloudinary.com/thm/image/upload/v1665384164/lc/olo_twykct.png"
-            alt="OLO pics studio"
-          >
-          <div class="text-grey-800 text-12 font-extrabold uppercase">
-            OLO pics
-          </div>
-        </client-only>
-      </nuxt-link>
-      <div class="hidden m:grid gap-12 grid-flow-col items-center justify-self-end">
-        <div class="grid gap-4 grid-flow-col items-center">
-          <a
-            v-for="(externalLink, i) in externalLinks"
-            :key="`externalLink-${i}`"
-            :href="externalLink.url"
-            target="blank"
-            class="
-              px-8
-              py-2
-              text-grey-600
-              typo-ui-m
-              bg-primary
-              bg-opacity-0
-              rounded-5
-              transition-default
-              duration-100
-              hover:text-primary
-              hover:bg-opacity-5
-              focus:text-primary
-              focus:bg-opacity-5
-            "
-          >
-            {{ externalLink.name }}
-          </a>
-        </div>
-        <div class="grid gap-4 grid-flow-col items-center">
-          <a
-            v-for="(socialLink, i) in socialLinks"
-            :key="`socialLink-${i}`"
-            :href="socialLink.url"
-            target="blank"
-            class="
-              relative
-              grid
-              place-items-center
-              w-32
-              h-32
-              before:content-['']
-              before:absolute
-              before:left-0
-              before:top-0
-              before:w-full
-              before:h-full
-              before:rounded-full
-              before:bg-current
-              before:opacity-0
-              before:transition-opacity
-              before:duration-100
-              hover:before:opacity-10
-              focus:before:opacity-10
-            "
-            :style="{ color: socialLink.color }"
-          >
-            <UtilsIcon
-              :name="socialLink.icon"
-              class="w-20 h-20"
-            />
-          </a>
-        </div>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="/style.css">
+  <header class="sticky top-0 py-10">
+<div class="container">
+<div class="row justify-content-center" style="margin: auto;">
+<div class="gap-12 grid-flow-col items-center justify-self-end">
+
         <client-only>
           <template v-if="isLoggedIn && truncatedAddress">
-            <ControlsButtonAction version="secondary">
+            <ControlsButtonAction type="submit" class="form-control btn btn-primary submit px-3" @click="emit(events.POPUP_GUARD)">
               {{ truncatedAddress }}
             </ControlsButtonAction>
-            <ControlsButtonIcon
-              version="error"
-              icon="Logout"
-              @click="emit(events.POPUP_GUARD)"
-            />
           </template>
-          <ControlsButtonAction
+          <ControlsButtonAction type="submit" class="form-control btn btn-primary submit px-3"
             v-else
             @click="emit(events.POPUP_GUARD)"
           >
@@ -95,130 +22,28 @@
           </ControlsButtonAction>
         </client-only>
       </div>
-      <ControlsButtonIcon
-        class="m:hidden"
-        icon="Menu"
-        @click="isMenuOpen = !isMenuOpen"
-      />
-    </Container>
-  </header>
-  <div
-    class="fixed top-0 left-0 z-1 m:hidden w-screen h-screen py-10 bg-white overflow-hidden shadow transition-transform duration-750"
-    :class="isMenuOpen ? 'translate-y-0' : '-translate-y-full'"
-  >
-    <Container class="grid gap-40 grid-rows-auto-1fr h-full">
-      <ControlsButtonIcon
-        class="justify-self-end"
-        icon="Cross"
-        @click="isMenuOpen = !isMenuOpen"
-      />
-      <div class="grid gap-32 content-center items-center">
-        <div class="grid gap-4 justify-items-center items-center">
-          <a
-            v-for="(externalLink, i) in externalLinks"
-            :key="`externalLink-${i}`"
-            :href="externalLink.url"
-            target="blank"
-            class="
-              px-8
-              py-2
-              text-grey-600
-              typo-ui-m
-              bg-primary
-              bg-opacity-0
-              rounded-5
-              transition-default
-              duration-100
-              hover:text-primary
-              hover:bg-opacity-5
-              focus:text-primary
-              focus:bg-opacity-5
-            "
-          >
-            {{ externalLink.name }}
-          </a>
-        </div>
-        <div class="grid gap-4 grid-flow-col justify-center items-center">
-          <a
-            v-for="(socialLink, i) in socialLinks"
-            :key="`socialLink-${i}`"
-            :href="socialLink.url"
-            class="
-              relative
-              grid
-              place-items-center
-              w-32
-              h-32
-              before:content-['']
-              before:absolute
-              before:left-0
-              before:top-0
-              before:w-full
-              before:h-full
-              before:rounded-full
-              before:bg-current
-              before:opacity-0
-              before:transition-opacity
-              before:duration-100
-              hover:before:opacity-10
-              focus:before:opacity-10
-            "
-            :style="{ color: socialLink.color }"
-          >
-            <UtilsIcon
-              :name="socialLink.icon"
-              class="w-20 h-20"
-            />
-          </a>
-        </div>
-        <div class="grid gap-12 grid-flow-col justify-center">
-          <client-only>
-            <template v-if="isLoggedIn && truncatedAddress">
-              <ControlsButtonAction version="secondary">
-                {{ truncatedAddress }}
-              </ControlsButtonAction>
-              <ControlsButtonIcon
-                version="error"
-                icon="Logout"
-                @click="emit(events.POPUP_GUARD)"
-              />
-            </template>
-            <ControlsButtonAction
-              v-else
-              @click="emit(events.POPUP_GUARD)"
-            >
-              Connect wallet
-            </ControlsButtonAction>
-          </client-only>
-        </div>
       </div>
-    </Container>
-  </div>
+      </div>
+  </header>
 </template>
-
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
 import { useImagesStore } from '@/stores/images'
 import { useUserStore } from '@/stores/user'
 import type { IconsNames } from '@/composables/useIconsComponents'
-
 type ExternalLink = {
   name: string
   url: string
 }
-
 type SocialLink = {
   icon: IconsNames
   url: string
   color: string
 }
-
 const { images } = storeToRefs(useImagesStore())
 const { isLoggedIn, truncatedAddress } = storeToRefs(useUserStore())
 const { emit, events } = useEventsBus()
-
 const { collection, scoreAddress } = useRuntimeConfig()
-
 const isMenuOpen = ref<boolean>(false)
 const title = ref<string>(collection)
 const externalLinks = ref<ExternalLink[]>([
