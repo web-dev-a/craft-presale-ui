557d5527d2c32ee77fe112be0e42d1ca4e175059
web-dev-a <61269281+web-dev-a@users.noreply.github.com>
2023-05-03 11:27:51 +0300

    Update PresaleMint.vue

--- components/partials/popup/action/PresaleMint.vue
+++ components/partials/popup/action/PresaleMint.vue
@@ -100,7 +100,7 @@ const { ICONEX_HANDLE_CANCEL } = useIconexListener()
 const { dipsatchLedger } = useLedgerStore()
 const { address, wallet } = storeToRefs(useUserStore())
 
-const nid = iconNetwork === 'testnet' ? '83' : '1'
+const nid = iconNetwork === 'testnet' ? '2' : '1'
 
 const isGlobalListening = ref<boolean>(false)
 const ACTION_PRESALEMINT = reactive<ActionData>({
